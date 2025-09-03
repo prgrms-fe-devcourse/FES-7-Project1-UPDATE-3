4c7885901993436a8ce3c7a9e10cdc7901f89eaf
Sage <prestoferoce@gmail.com>
2025-09-03 12:50:50 +0900

    feat: request 에러 처리 추가

--- src/api/http.js
+++ src/api/http.js
@@ -1,25 +1,30 @@
 const request = async ({ url, method = "GET", headers = {}, body }) => {
   const options = {
     method,
-    headers,
+    headers: { ...headers },
   };
 
-  if (body) {
+  if (body !== undefined) {
     options.body = JSON.stringify(body);
-    headers["Content-Type"] = "application/json";
+    options.headers["Content-Type"] = "application/json";
   }
 
-  const response = await fetch(url, options);
-  return response.json();
+  try {
+    const response = await fetch(url, opts);
+
+    if (!response.ok) throw new Error("Request Error");
+
+    return await response.json();
+  } catch {
+    throw new Error("Request Error");
+  }
 };
 
 request.get = (url, headers) => request({ url, method: "GET", headers });
 
-request.post = (url, headers, body) =>
-  request({ url, method: "POST", body, headers });
+request.post = (url, headers, body) => request({ url, method: "POST", body, headers });
 
-request.put = (url, headers, body) =>
-  request({ url, method: "PUT", body, headers });
+request.put = (url, headers, body) => request({ url, method: "PUT", body, headers });
 
 request.delete = (url, headers) => request({ url, method: "DELETE", headers });
 
