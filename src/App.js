4c5ad5576b4c78bdc31f3652a26adf81dfb75016
rumpumpum <kjh04215@gmail.com>
2025-09-04 04:53:02 +0900

    feat: App.js 에 editor 추가

--- src/App.js
+++ src/App.js
@@ -7,19 +7,10 @@ export default function App() {
   const router = createRouter();
 
   const main = document.createElement("main");
-<<<<<<< HEAD
-<<<<<<< HEAD
-  main.appendChild(Sidebar());
-  main.appendChild(Editor());
-=======
-  main.appendChild(await Sidebar());
->>>>>>> a8becbf (이벤트 리스너를 이벤트 위임 형식으로 변경하고 sidebarEl에 적용)
-=======
   Sidebar().then((sidebarEl) => {
     main.appendChild(sidebarEl);
   });
-  // main.appendChild(Sidebar());
->>>>>>> dae8d51 (App.js에서 then을 이용하여 promise 반환값 처리)
+  main.appendChild(Editor());
 
   router.addRoute("/", "empty");
   router.addRoute("/documents", "empty");
