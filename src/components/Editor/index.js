a371c8c1844d065438f5862c2bbc1c80fdd682d3
Byeonsooyeon <xjkooy100@gmail.com>
2025-09-03 22:24:59 +0900

    feat : App.js에 Editor 연결, id 추가, 더미 데이터 추가(임시 id)

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -23,9 +23,10 @@ const TEST_DOCUMENTS = [
   },
 ];
 
-export function Editor() {
+export default function Editor() {
   /* editor 기본 구조 생성 */
   const section = document.createElement("section");
+  section.id = "section";
   // title과 contents를 각각의 div로 구성
   const title = document.createElement("div");
   const contents = document.createElement("div");
@@ -36,7 +37,9 @@ export function Editor() {
   section.appendChild(contents);
 
   /* url 에서 id 가져오기 */
-  const id = window.location.pathname.split("/")[2];
+  //const id = window.location.pathname.split("/")[2];
+  // test용
+  const id = "1";
 
   /* 문서 내용 렌더링 */
   // 가져온 id를 통해 데이터를 탐색
@@ -44,14 +47,14 @@ export function Editor() {
 
   // 텍스트 입력 가능, title/contents 내용이 없는 경우 포함
   const titleText = document.createElement("textarea");
-  if (testData.title !== null) titleText.value = testData.title;
-  else if (testData.title === null) titleText.value = "새 페이지";
-  //titleText.textContent = "새 페이지";
+  titleText.id = "titleText";
+  if (testData?.title !== null) titleText.value = testData.title;
+  else if (testData?.title === null) titleText.value = "새 페이지";
 
   const contentsText = document.createElement("textarea");
+  contentsText.id = "contentsText";
   if (testData.content !== null) contentsText.value = testData.content;
   else if (testData.content === null) contentsText.value = "내용을 입력하세요.";
-  //contentsText.textContent = "내용을 입력하세요.";
 
   title.appendChild(titleText);
   contents.appendChild(contentsText);
